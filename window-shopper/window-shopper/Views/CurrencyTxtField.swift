f2d7f0141e77204575ce28ac8b6e1d178f0a0539
Gustavobbs <36171903+Gustavobbs@users.noreply.github.com>
2020-03-27 11:33:07 -0300

    Initial interface and view model for textField

---
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,28 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Gustavo Buoro Branco de Souza on 27/03/20.
+//  Copyright © 2020 Gustavo Buoro Branco de Souza. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+
+}
